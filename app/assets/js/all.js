824cf5f8512d2e3cc68d3968169bb14b2da5ab25
tanukili <108317269+tanukili@users.noreply.github.com>
2022-11-29 11:36:13 +0800

    增加 axios 套件

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,4 +1,4 @@
-let data = [
+/*let data = [
   {
     "id": 0,
     "name": "肥宅心碎賞櫻3日",
@@ -29,36 +29,48 @@ let data = [
     "price": 1765,
     "rate": 7
   }
-];
+];*/
 
 const productList = document.querySelector('.product');
-//資料初始值
-let str = '';
-//累加資料
-data.forEach(function(item){
-  str += `<li class="gx-4 position-r bg-light mb-38 product-s-r">
-  <img src="${item.imgUrl}" alt="travel-01" class="list-img">
-  <p class="font-2-2 second-style position-area pt-8 pb-8 pr-20 pl-20 tag-r">${item.area}</p>
-  <p class="font-1-1 position-score main-style pt-5 pb-5 tac tag-r">${item.rate}</p>
-  <div class="p-20 list-content df fxdc jcsb">
-    <div>
-      <h2 class="font-3-4-m primary pb-4 border-2 mb-16">${item.name}</h2>
-      <p class="font-1-2 font-light mb-22">${item.description}</p>
-    </div>
-    <div class="df jcsb aic primary">
-      <div class="df">
-        <span class="material-icons-outlined mr-6">
-          error
-        </span>
-        <p class="font-1-1-m">剩下最後 ${item.group} 組</p>
-      </div>
-      <div class="df aic">
-        <p class="font-1-1-m mr-4">TWD</p>
-        <p class="font-4-5-m ff-en">$${item.price}</p>
-      </div>
-    </div>
-  </div>
-</li>`;
-});
-//畫出資料
-productList.innerHTML = str;
+//新增資料筆數
+const seachNum = document.querySelector('.seachNum');
+
+axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+  .then(function(response){
+    //多了一個外層物件的 data
+    let data = response.data.data;
+    //資料初始值
+    let str = '';
+    data.forEach(function(item,index){
+      str += `<li class="gx-4 position-r bg-light mb-38 product-s-r">
+                <img src="${item.imgUrl}" alt="travel-01" class="list-img">
+                <p class="font-2-2 second-style position-area pt-8 pb-8 pr-20 pl-20 tag-r">${item.area}</p>
+                <p class="font-1-1 position-score main-style pt-5 pb-5 tac tag-r">${item.rate}</p>
+                <div class="p-20 list-content df fxdc jcsb">
+                  <div>
+                    <h2 class="font-3-4-m primary pb-4 border-2 mb-16">${item.name}</h2>
+                    <p class="font-1-2 font-light mb-22">${item.description}</p>
+                  </div>
+                  <div class="df jcsb aic primary">
+                    <div class="df">
+                      <span class="material-icons-outlined mr-6">
+                        error
+                      </span>
+                      <p class="font-1-1-m">剩下最後 ${item.group} 組</p>
+                    </div>
+                    <div class="df aic">
+                      <p class="font-1-1-m mr-4">TWD</p>
+                      <p class="font-4-5-m ff-en">$${item.price}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>`;
+      seachNum.textContent = `本次搜尋共 ${index + 1} 筆資料`
+    });
+    //畫出資料
+    productList.innerHTML = str;
+  })
+
+
+
+
